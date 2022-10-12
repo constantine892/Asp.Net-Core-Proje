ae8a170efd44137a2ba465b7ac32205aafbc3b5c
Ramazan Taşkın <ramazan892.123@gmail.com>
2022-10-12 18:00:20 +0300

    12.10.2022

--- proje 2/Controllers/BlogContorollers.cs
+++ proje 2/Controllers/BlogContorollers.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
 
 namespace proje_2.Controllers
 {
     public class BlogContorollers : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
+            var Values = bm.GetList();
             return View();
         }
     }
